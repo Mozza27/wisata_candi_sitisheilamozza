18121f7ea5a6f5ab2af4774123e7f2856f159a54
Mozza27 <sitisheilamozza_2226240168@mhs.mdp.ac.id>
2024-11-04 13:09:11 +0700

    signinscreen

--- wisata_candi_sitisheilamozza-main/lib/main.dart
+++ wisata_candi_sitisheilamozza-main/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/screens/sign_in_screen.dart';
+import 'screens/profile_screen.dart';
+import 'data/candi_data.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,20 +17,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Wisata Candi',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        appBarTheme: const AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.deepPurple),
+          titleTextStyle: TextStyle(
+            color: Colors.deepPurple,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        colorScheme:
+        ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
+          primary: Colors.deepPurple,
+          surface: Colors.deepPurple[50],
+        ),
         useMaterial3: true,
-      ), //ThemeData
-      home: DetailScreen(),
-    ); // MaterialApp
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
-
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+      ),
+      // home: const ProfileScreen(),
+      // home: DetailScreen(candi: candiList[0]),
+      home: SignInScreen(),
+    );
   }
 }
--- wisata_candi_sitisheilamozza-main/lib/screens/profile_screen.dart
+++ wisata_candi_sitisheilamozza-main/lib/screens/profile_screen.dart
@@ -8,46 +8,56 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  // TODO : deklarasikan variabel yang dibutuhkan
-  bool isSignedIn = false;
-  String fullName = '';
-  String userName = '';
+
+  bool isSignedIn = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           Container(
-            height: 200,
-            width: double.infinity,
+            height: 200, width: double.infinity,
             color: Colors.deepPurple,
           ),
           Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                children: [
-                  //TODO:2, buat bagian gambar header yang berisi gambar profile
-                  Stack(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Color.deepPurple,width: 2),
-                          shape: BoxShape.circle,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                //   TODO 2. Buat bagian ProfileHeader yang berisi gambar profil
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 200-50),
+                    child: Stack(
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.deepPurple, width: 2),
+                            shape: BoxShape.circle,
+                          ),
+                          child: CircleAvatar(
+                            radius: 50,
+                            backgroundImage:
+                            AssetImage('images/placeholder_image.png'),
+                          ),
                         ),
-                        child: CircleAvatar(
-                          radius: 50,
-                          backgroundImage: AssetImage(''),
-                        ),
-                      )
-                    ],
-                  )
-                  //TODO:3, buat bagian profileinfo yang berisi info profile
-                  //TODO:4, buat bagian profileaction yang berisi textButton
-                 ],
-              ),
-           )
+                        if(isSignedIn)
+                          IconButton(
+                            onPressed: (){},
+                            icon: Icon(Icons.camera_alt, color: Colors.deepPurple[50],),
+                          ),
+                      ],
+                    ),
+                    //   TODO 3. Buaqt bagian ProfileInfo yang berisi info profile
+                    //   TODO 4. buat ProfileActions yang berisi TextButton sign in/out
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
-}
